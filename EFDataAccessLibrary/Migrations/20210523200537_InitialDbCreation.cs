ff52a52472537e585b63ac98eaa1f8b5ffe60815
kubilayeldemir <kubi0707@gmail.com>
2021-05-24 14:49:00 +0300

    Add project files.

---
+++ EFDataAccessLibrary/Migrations/20210523200537_InitialDbCreation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccessLibrary.Migrations
+{
+    public partial class InitialDbCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StreetAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmailAdress = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailAddresses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "EmailAddresses");
+        }
+    }
+}
---
+++ EFDataAccessLibrary/Migrations/20210524113846_people-table.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccessLibrary.Migrations
+{
+    public partial class peopletable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersonId",
+                table: "EmailAddresses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PersonId",
+                table: "Addresses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailAddresses_PersonId",
+                table: "EmailAddresses",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_PersonId",
+                table: "Addresses",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_People_PersonId",
+                table: "Addresses",
+                column: "PersonId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmailAddresses_People_PersonId",
+                table: "EmailAddresses",
+                column: "PersonId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_People_PersonId",
+                table: "Addresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmailAddresses_People_PersonId",
+                table: "EmailAddresses");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EmailAddresses_PersonId",
+                table: "EmailAddresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_PersonId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "EmailAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "Addresses");
+        }
+    }
+}
